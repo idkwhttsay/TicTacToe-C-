f1090804ff5e9d960bf54a990510ea780bf0efd5
idkwhttsay <danil.tatarinov.00@gmail.com>
2024-05-07 13:40:50 +0500

    first commit

---
+++ TicTacToe/Player.cpp
@@ -0,0 +1,10 @@
+//
+//  Player.cpp
+//  TicTacToe
+//
+//  Created by DanTatar on 07.05.2024.
+//
+
+#include "Player.hpp"
+
+Player::Player(char c) : symbol(c) {}
---
+++ TicTacToe/Player.hpp
@@ -0,0 +1,19 @@
+//
+//  Player.hpp
+//  TicTacToe
+//
+//  Created by DanTatar on 07.05.2024.
+//
+
+#ifndef Player_hpp
+#define Player_hpp
+
+class Player {
+public:
+    char symbol;
+    Player(char c);
+private:
+    
+};
+
+#endif /* Player_hpp */
